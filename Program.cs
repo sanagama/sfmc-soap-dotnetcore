fa62c1cb28584ea759aa1e15856a9f9a097903ad
sanagama <sanagama2@gmail.com>
2018-04-10 12:18:31 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sfmc_soap_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
